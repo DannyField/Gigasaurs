b49ab6d5547ffc6fa1336cd1622d9fc6afe7ed0b
DannyField <dannyfieldproductions@gmail.com>
2020-05-21 12:24:58 +1000

    code changes and fixes, and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     
+    # A user can perform at many gigs, so the association is a one to many
     has_many :gigs
+
+    # A user may perform in many bands. So a one to many association is created.
     has_many :bands
+
+    # A user may use Active Storage to upload an image, so this eager loading is used.
     has_one_attached :profile_photo
 
+    # A band that the user belongs too, may also have other band members, so a many to many join table is used.
     has_many :band_users
     has_many :bands, through: :band_users
-
-  # Not sure about this line, as I originally set it up as bands can create gigs only
 
   #   def admin?
   #     if self.email.ends_with('@gigasaurs.com')
