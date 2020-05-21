a3ed3df5e00c0c9ecb4ac3d705a81630e1034b3d
DannyField <dannyfieldproductions@gmail.com>
2020-05-21 14:04:41 +1000

    code comments and checks

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,13 +1,15 @@
 class BandsController < ApplicationController
-before_action :find_band ,only: [:show, :edit, :update, :destroy]
-before_action :authenticate_user! ,only: [:new, :create, :edit, :update, :destroy]
-# load_and_authorize_resource
-
+    # Run the method find_bnd (in private) before all else, but only for show, edit, update and destroy 
+    before_action :find_band ,only: [:show, :edit, :update, :destroy]
+    
+    # All users need to login.
+    before_action :authenticate_user! ,only: [:new, :create, :edit, :update, :destroy, :index]
+    
+    # Cancancan authorise method. Currently disabled for this release
+    # load_and_authorize_resource
 
     def index
-        authenticate_user!
         @bands = Band.all
-        # @bands = Band.search(params[:search]) 
     end
 
     def show
@@ -19,11 +21,12 @@ before_action :authenticate_user! ,only: [:new, :create, :edit, :update, :destro
     end
 
     def create 
-        # Ed helped with this code. So I know it works
+        # Ed helped with this code. So we know it works.
         @band = Band.new(band_params)
-        # My god, I needed this to save the current user. It doesn't overwrite, as its @band.user
+        # My god, I needed this to save the current user. It doesn't overwrite, as its @band.user.
+        # It runs down the list, as typical ruby. The @band is the object, but the @band.user is an attribute of the @band
         @band.user = current_user
-        # can change to email if I want the creator to add the right person
+        # can change to email if I want the creator to add the right person. Will do this for next release.
         band_member = User.where(name: params[:band][:band_member])
 
         unless band_member.empty?
@@ -56,12 +59,15 @@ before_action :authenticate_user! ,only: [:new, :create, :edit, :update, :destro
         redirect_to bands_path
     end
 
-private
+    # Private keeps the code below safe from external manipulation
+    private
 
+    # Passes param ID (i.e. Band id), found in the Band table. Can be called by any method above now. In this case, it is called at the very start. See line 3
     def find_band
         @band = Band.find(params[:id])
     end
 
+    # Band params, whitelisting and allowing these attributes to be used.
     def band_params
         params.require(:band).permit(:name, :profile_picture, :about, :website)
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+# Run the method find_user (in private) before all else, but only for show, edit, update and destroy 
 before_action :find_user ,only: [:show, :edit, :update, :destroy]
+
+# All users need to login.
 before_action :authenticate_user!
+
+# Cancancan authorise method. Currently disabled for this release
 # load_and_authorize_resource
 
     def index 
@@ -10,28 +15,25 @@ before_action :authenticate_user!
     def show
         @user = User.find(params[:id])
     end
-    
-        def new
 
+        # Creates a new User object; initialises an empty user object in memory.
+        def new
             @user = User.new
-            
+
         end
     
         def create
             @user = User.new(user_params)
             @user.save
-    
             redirect_to @user
-    
-            # I will most likely remove this as it should only be a user that can create a band
         end
     
         def edit
+            # This line isn't needed as it's now called within the method find_gig, and before_action.
             # @user = User.find(params[:id])
         end
     
         def update
-            # @user = User.find(params[:id])
             @user.update(user_params)
     
             redirect_to @user
@@ -43,13 +45,15 @@ before_action :authenticate_user!
             redirect_to user_path
         end
     
-    
+        # Private keeps the code below safe from external manipulation
         private
-    
+
+        # Passes param ID (i.e. user ID), found in the User table. Can be called by any method above now. Inthis case, it is called at the very start. See line 3   
         def find_user
             @user = User.find(params[:id])
         end
     
+        # User params, whitelisting and allowing these attributes to be used.
         def user_params
             params.require(:user).permit(:name, :profile_photo, :about, :location)
         end
